58ad3ff56a44986f0381fd9f245741cd59ff3d50
Alexandre Joris <alex@blockchainpartner.fr>
2022-06-20 19:59:24 +0200

    :wrench: added Choices to incident model + textfield for description

--- incidents/app/models.py
+++ incidents/app/models.py
@@ -2,12 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Incident(models.Model):
+    class Severity(models.TextChoices):
+        LOW = 'LOW', 'Low'
+        MEDIUM = 'MEDIUM', 'Medium'
+        HIGH = 'HIGH', 'High'
+
+    class Status(models.TextChoices):
+        OPEN = 'OPEN', 'Open'
+        CLOSED = 'CLOSED', 'Closed'
+
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
+    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=100)
-    severity = models.CharField(max_length=100)
+    status = models.CharField(
+        max_length=6,
+        choices=Status.choices,
+    )
+    severity = models.CharField(
+        max_length=6,
+        choices=Severity.choices,
+    )
 
     class Meta:
         ordering = ['-created_at']
