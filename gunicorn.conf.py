b62276e179b205a6879a071f2f954bf476d1038b
Alexandre Joris <shad@chaz.pro>
2023-02-11 00:17:54 +0100

    added opentelemetry imports to gunicorn conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -21,3 +21,6 @@ class RequestPathFilter(logging.Filter):
 def on_starting(server):
     server.log.access_log.addFilter(RequestPathFilter(path_re=r'^/health/$'))
     server.log.access_log.addFilter(RequestPathFilter(path_re=r'^/metrics/$'))
+
+def post_fork(server, worker):
+    from opentelemetry.instrumentation.auto_instrumentation import sitecustomize
